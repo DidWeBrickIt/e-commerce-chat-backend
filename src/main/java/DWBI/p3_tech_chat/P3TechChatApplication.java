7bf3efd8e52796bb669d45cc49b701a07ddcafa6
Sarah <108547842+Sarah-C-F@users.noreply.github.com>
2022-09-19 13:48:04 -0700

    added original files

---
+++ src/main/java/DWBI/p3_tech_chat/P3TechChatApplication.java
@@ -0,0 +1,13 @@
+package DWBI.p3_tech_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3TechChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3TechChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DWBI/p3_tech_chat/P3TechChatApplicationTests.java
@@ -0,0 +1,13 @@
+package DWBI.p3_tech_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3TechChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
